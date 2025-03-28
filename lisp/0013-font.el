4f0d65268d482570698eb7b2e9c42f8e01886919
troy <troyemails@gmail.com>
2025-03-28 13:35:03 -0700

    size tweaks for pgtk

--- lisp/0013-font.el
+++ lisp/0013-font.el
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Add it
-(add-to-list 'default-frame-alist '(font . "Hack Nerd Font Mono-16"))
+(add-to-list 'default-frame-alist '(font . "Hack Nerd Font Mono-10"))
 (set-face-attribute 'default nil
 		    :family "Hack Nerd Font Mono"
-		    :height 160)
+		    :height 100)
