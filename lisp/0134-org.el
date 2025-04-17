ba778605b00475a6b2cc012c58adee9f5c9912a1
troy <troy.fredriks@qsc.com>
2025-04-16 21:28:24 -0700

    first pass at new agenda view with working refile targets font change on heading depth

--- lisp/0134-org.el
+++ lisp/0134-org.el
@@ -22,13 +22,18 @@
 ;; Set agenda directory to org-lyfe if it exists
 ;; https://stackoverflow.com/questions/11384516/how-to-make-all-org-files-under-a-folder-added-in-agenda-list-automatically
 (if (file-exists-p org-lyfe-d)
-    ;; (setq org-agenda-files (append
-    ;; 			    (list (concat org-lyfe-d "inbox.org"))
-    ;; 			    (directory-files-recursively org-lyfe-d "\\todo.org$")))
-    (setq org-agenda-files (append
-			    `(,org-lyfe-d)
+    (setq org-agenda-files (cons
+			    (concat org-lyfe-d "inbox.org")
 			    (directory-files-recursively (concat org-lyfe-d "projects/") "\\todo.org$")))
   )
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "|" "DONE(d)" "|" "Cancelled(c)")))
+
+(setq org-tag-alist '(("@errand" . ?e)
+                      ("@office" . ?o)
+                      ("@home"   . ?h)))
+
 ;; start with indent mode
 (setq org-startup-indented t)
 
@@ -55,6 +60,67 @@
         ("c" "org-protocol-capture" entry (file ,(concat org-lyfe-d "inbox.org"))
          "* TODO [[%:link][%:description]]\n\n %i" :immediate-finish t)))
 
+;; Resize headings
+(custom-set-faces
+  '(org-document-title ((t (:height 2.0 :weight bold))))
+  '(org-level-1 ((t (:inherit outline-1 :height 1.7 :spacing 2))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.6 :spacing 2))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
+  '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
+  '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
+
+;; Hide emphasis markers in *bold* or /italic/
+(setq org-hide-emphasis-markers 't)
+
+;; When a section is collapsed, this is display instead of '...'
+(setq org-ellipsis " ▾ ")
+
+;; Configure the custom org agenda view
+(setq org-agenda-custom-commands
+      '((" " "Agenda"
+        ((agenda ""
+                 ((org-agenda-span 1)
+                  (org-agenda-start-on-weekday nil)
+                  (org-agenda-start-day "+0d")
+                  (org-deadline-warning-days 365)))
+
+         (todo "TODO"
+               ((org-agenda-overriding-header " To Refile")
+                (org-agenda-files (list (concat org-lyfe-d "inbox.org")))))
+
+         (todo "NEXT"
+               ((org-agenda-overriding-header " In Progress")
+                (org-agenda-files (cons
+                                   (concat org-lyfe-d "todo.org")
+                                   (directory-files-recursively (concat org-lyfe-d "projects/") "\\todo.org$")
+				   ))))
+
+         (todo "TODO"
+               ((org-agenda-overriding-header " Project Tasks")
+                (org-agenda-files (directory-files-recursively (concat org-lyfe-d "projects/") "\\todo.org$"))))
+
+         (todo "TODO"
+               ((org-agenda-overriding-header " One Off Tasks")
+                (org-agenda-files (list (concat org-lyfe-d "todo.org")))))
+
+	 (todo "HOLD"
+               ((org-agenda-overriding-header " Blocked Tasks")
+                (org-agenda-files  (cons
+				    (concat org-lyfe-d "todo.org")
+                                    (directory-files-recursively (concat org-lyfe-d "projects/") "\\todo.org$")
+				    ))))
+         nil))))
+
+;; Configure refiling menu when refiling tasks
+(setq org-refile-use-outline-path 'full-file-path
+     org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)  ;; Allow creation of headings under which to group tasks, when refiling them
+(setq org-refile-targets  `(
+			   (,(concat org-lyfe-d "todo.org") . (:level . 0))
+			   (,(directory-files-recursively (concat org-lyfe-d "projects/") "\\todo.org$") . (:level . 0))
+			   ))
 
 
 ;; keybinds
